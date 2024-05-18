eb75a9907f2b576f849a72a9176df0ebff311387
Alex Alikiotis <alexalikiotis5@gmail.com>
2024-05-18 23:20:15 +0300

    Adding timestamp update trigger to postgres

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
+-- Creates the `accounts` table.
 CREATE TABLE accounts (
   id UUID NOT NULL DEFAULT gen_random_uuid(),
   name VARCHAR(30) NOT NULL,
@@ -7,6 +8,7 @@ CREATE TABLE accounts (
   PRIMARY KEY (id)
 );
 
+-- Creates the `transactions` table.
 CREATE TABLE transactions (
   id SERIAL NOT NULL,
   sender UUID NOT NULL,
@@ -16,6 +18,22 @@ CREATE TABLE transactions (
   PRIMARY KEY (id)
 );
 
+-- Creates a procedure to automatically update the `updated_at`
+-- field with the current timestamp on row updates.
+CREATE OR REPLACE FUNCTION update_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON accounts
+FOR EACH ROW
+EXECUTE FUNCTION update_timestamp();
+
+-- Populates the DB with some data.
 INSERT INTO accounts (name, balance) VALUES ('Charles Carmichael', 1000.50);
 INSERT INTO accounts (name, balance) VALUES ('Don Quixote', 500.0);
 INSERT INTO accounts (name, balance) VALUES ('Star-Lord', 1000000.90);
