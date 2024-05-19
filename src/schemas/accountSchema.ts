7c8c8a358c8a6bae4bd2fcc489b343bbbcc629e9
Alex Alikiotis <alexalikiotis5@gmail.com>
2024-05-19 16:34:24 +0300

    Updating accountSchema requirements

--- src/schemas/accountSchema.ts
+++ src/schemas/accountSchema.ts
@@ -5,5 +5,5 @@ export const accountByIdSchema = z.string().uuid('Not a valid UUID value.');
 export const accountCreationSchema = z.object({
   fullName: z.string().max(30, 'Name must be max 30 characters.'),
   email: z.string().email('Not a valid email address.'),
-  balance: z.number(),
+  balance: z.number().optional(),
 });
